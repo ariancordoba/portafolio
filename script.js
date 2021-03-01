16d2e933588d3fe809e416f87caf90f2360c50e3
Arian Cordoba <arian.gasolero@gmail.com>
2021-03-01 02:49:19 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// typing
+
+var typed = new Typed(".typing", {
+  strings: ["Desarrollador Front-End"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+var typed = new Typed(".typing2", {
+  strings: ["Desarrollador Web", "Editor de Videos"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
